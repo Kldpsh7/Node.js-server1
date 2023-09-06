1986688d941241e30764af837f94c0de6e852a73
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-09-06 15:10:26 +0530

    form added

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const http=require('http')
 const server=http.createServer((req,res)=>{
     let url=req.url;
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>First html Page</title></head>');
-    if (url=='/' || url=='/home'){
-        res.write('<body><h1 style="text-align:center; color:green">Welcome to Home Page</h1></body>');
+    if (url==='/'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>First html Page</title></head>');
+        res.write('<body><form action="/message" method="post" ><input type="text" name="message" placeholder="Enter Message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
     }
-    else if (url=='/about'){
-        res.write('<body><h1 style="text-align:center; color:green">Welcome to About-Us Page</h1></body>');
+    else{
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>First html Page</title></head>');
+        res.write('<body><h1 style="text-align:center; color:green">Welcome to message Page</h1></body>');
+        res.write('</html>');
+        return res.end();
     }
-    else if (url=='/node'){
-        res.write('<body><h1 style="text-align:center; color:green">Welcome to Node JS Project</h1></body>');
-    }
-    res.write('</html>');
-    res.end();
+
 });
 server.listen(4000)
