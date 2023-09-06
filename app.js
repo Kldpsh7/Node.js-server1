081f4bd5d183a2694ad93775b09766181d0946f5
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-09-06 14:37:00 +0530

    Added html response

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const http=require('http')
 const server=http.createServer((req,res)=>{
-    console.log('Kuldeep Sharma');
+    let url=req.url;
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>First html Page</title></head>');
+    if (url=='/' || url=='/home'){
+        res.write('<body><h1 style="text-align:center; color:green">Welcome to Home Page</h1></body>');
+    }
+    else if (url=='/about'){
+        res.write('<body><h1 style="text-align:center; color:green">Welcome to About-Us Page</h1></body>');
+    }
+    else if (url=='/node'){
+        res.write('<body><h1 style="text-align:center; color:green">Welcome to Node JS Project</h1></body>');
+    }
+    res.write('</html>');
+    res.end();
 });
 server.listen(4000)
